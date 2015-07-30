72f89604b939cb2a9b5c5d580749084f303362ba
BhanuPrakashCalitate <bhanuprakashbl003@gmail.com>
2015-07-30 12:35:43 +0530

    Changes made

--- src/com/suite/regression/UserGroup.java
+++ src/com/suite/regression/UserGroup.java
@@ -1,7 +1,10 @@
 package com.suite.regression;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -10,106 +13,114 @@ import org.testng.annotations.Test;
 import com.utils.Constants;
 import com.utils.Utils;
 
-public class UserGroup extends RegressionSuiteBase{
+public class UserGroup extends RegressionSuiteBase {
 	WebElement source, destination, userSearchResult;
-	
-  @BeforeTest
-  public void login(){
-	  getElementByXpath("textUserName").sendKeys(Constants.USERNAME);
-	  getElementByXpath("textPassWord").sendKeys(Constants.PASSWORD, Keys.ENTER);
-  }
-  
-  @AfterTest
-  public void logout(){
-	  getElementByXpath("btnAdministrator").click();
-	  getElementByXpath("btnLogout").click();
-  }
-  
-  @Test(priority=1)
-  public void createEmptyUserGroup(){
-	  getElementByXpath("btnUserGroup").click();
-	  getElementByXpath("btnCreateUG").click();
-	  getElementByXpath("textUGName").sendKeys("UserGroup" + Utils.randGen());
-	  getElementByXpath("btnScheduleNow").click();
-  }
-  
-  @Test(priority=2)
-  public void addDefaultUser(){
-	  getElementByXpath("btnUserGroup").click();
-	  getElementByXpath("btnCreateUG").click();
-	  getElementByXpath("textUGName").sendKeys("UserGroup" + Utils.randGen());
-	  source = getElementByXpath("defaultUserSource");
-	  destination = getElementByXpath("userDestination");
-	  dragAndDrop(source, destination);
-	  getElementByXpath("btnScheduleNow").click();
-  }
-  
-  @Test(priority=3)
-  public void addCustomUser(){
-	  getElementByXpath("btnUserGroup").click();
-	  getElementByXpath("btnCreateUG").click();
-	  getElementByXpath("textUGName").sendKeys("UserGroup" + Utils.randGen());
-	  source = getElementByXpath("customUserSource");
-	  destination = getElementByXpath("userDestination");
-	  dragAndDrop(source, destination);
-	  getElementByXpath("btnScheduleNow").click();
-  }
-  
-  @Test(priority=4)
-  public void addUserBySearch(){
-	  getElementByXpath("btnUserGroup").click();
-	  getElementByXpath("btnCreateUG").click();
-	  getElementByXpath("textUGName").sendKeys("UserGroup" + Utils.randGen());
-	  getElementByXpath("userSearchField").sendKeys("fep", Keys.ENTER);
-	  
-	  int attempt = 0;
-	  while(attempt < 2){
-			  source = getElementByXpath("searchedUserSource");
-			  destination = getElementByXpath("userDestination");
-			  dragAndDrop(source, destination);
-			  attempt++;
-	  }
-	  getElementByXpath("btnScheduleNow").click();
-  }
-  
-  @Test(priority=5)
-  public void validateFormField(){
-	  getElementByXpath("btnUserGroup").click();
-	  getElementByXpath("btnCreateUG").click();
-	  getElementByXpath("btnScheduleNow").click();
-	  Assert.assertEquals(getElementByXpath("emptyUGName").getText(), "This field is required.");
-	  getElementByXpath("btnCancel").click();
-  }
-  
-  @Test(priority=6)
-  public void cancelCreatingUserGroup(){
-	  getElementByXpath("btnUserGroup").click();
-	  getElementByXpath("btnCreateUG").click();
-	  getElementByXpath("textUGName").sendKeys("UserGroup" + Utils.randGen());
-	  source = getElementByXpath("defaultUserSource");
-	  destination = getElementByXpath("userDestination");
-	  dragAndDrop(source, destination);
-	  getElementByXpath("btnCancel").click();
-  }
-  
-  @Test(priority=7)
-  public void deleteUserGroup(){
-	  getElementByXpath("checkBoxUserGroup").click();
-	  if(getElementByXpath("btnDeleteUserGroup").isEnabled()){
-		  getElementByXpath("btnDeleteUserGroup").click();
-	  }
-	  runJobSupervisor();
-  }
-  
-  @Test(priority=8)
-  public void deleteAllUserGroup() throws Exception{
-	  getElementByXpath("btnUserGroup").click();
-	  Utils.threadSleep(5000);
-	  getElementByXpath("checkBoxAllUserGroup").click();
-	  if(getElementByXpath("btnDeleteUserGroup").isEnabled()){
-		  getElementByXpath("btnDeleteUserGroup").click();
-	  }
-	  runJobSupervisor();
-  }
-  
+
+	@BeforeTest
+	public void login() throws Exception {
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get(Constants.URL);
+		readOR(Constants.PATHOBJECTREPO);
+		getElementByXpath("textUserName").sendKeys(Constants.USERNAME);
+		getElementByXpath("textPassWord").sendKeys(Constants.PASSWORD,
+				Keys.ENTER);
+	}
+
+	@AfterTest
+	public void logout() {
+		getElementByXpath("btnAdministrator").click();
+		getElementByXpath("btnLogout").click();
+		driver.quit();
+	}
+
+	@Test(priority = 1)
+	public void createEmptyUserGroup() {
+		getElementByXpath("btnUserGroup").click();
+		getElementByXpath("btnCreateUG").click();
+		getElementByXpath("textUGName").sendKeys("UserGroup" + Utils.randGen());
+		getElementByXpath("btnScheduleNow").click();
+	}
+
+	@Test(priority = 2)
+	public void addDefaultUser() {
+		getElementByXpath("btnUserGroup").click();
+		getElementByXpath("btnCreateUG").click();
+		getElementByXpath("textUGName").sendKeys("UserGroup" + Utils.randGen());
+		source = getElementByXpath("defaultUserSource");
+		destination = getElementByXpath("userDestination");
+		dragAndDrop(source, destination);
+		getElementByXpath("btnScheduleNow").click();
+	}
+
+	@Test(priority = 3)
+	public void addCustomUser() {
+		getElementByXpath("btnUserGroup").click();
+		getElementByXpath("btnCreateUG").click();
+		getElementByXpath("textUGName").sendKeys("UserGroup" + Utils.randGen());
+		source = getElementByXpath("customUserSource");
+		destination = getElementByXpath("userDestination");
+		dragAndDrop(source, destination);
+		getElementByXpath("btnScheduleNow").click();
+	}
+
+	@Test(priority = 4)
+	public void addUserBySearch() {
+		getElementByXpath("btnUserGroup").click();
+		getElementByXpath("btnCreateUG").click();
+		getElementByXpath("textUGName").sendKeys("UserGroup" + Utils.randGen());
+		getElementByXpath("userSearchField").sendKeys("fep", Keys.ENTER);
+
+		int attempt = 0;
+		while (attempt < 2) {
+			source = getElementByXpath("searchedUserSource");
+			destination = getElementByXpath("userDestination");
+			dragAndDrop(source, destination);
+			attempt++;
+		}
+		getElementByXpath("btnScheduleNow").click();
+	}
+
+	@Test(priority = 5)
+	public void validateFormField() {
+		getElementByXpath("btnUserGroup").click();
+		getElementByXpath("btnCreateUG").click();
+		getElementByXpath("btnScheduleNow").click();
+		Assert.assertEquals(getElementByXpath("emptyUGName").getText(),
+				"This field is required.");
+		getElementByXpath("btnCancel").click();
+	}
+
+	@Test(priority = 6)
+	public void cancelCreatingUserGroup() {
+		getElementByXpath("btnUserGroup").click();
+		getElementByXpath("btnCreateUG").click();
+		getElementByXpath("textUGName").sendKeys("UserGroup" + Utils.randGen());
+		source = getElementByXpath("defaultUserSource");
+		destination = getElementByXpath("userDestination");
+		dragAndDrop(source, destination);
+		getElementByXpath("btnCancel").click();
+	}
+
+	@Test(priority = 7)
+	public void deleteUserGroup() {
+		getElementByXpath("checkBoxUserGroup").click();
+		if (getElementByXpath("btnDeleteUserGroup").isEnabled()) {
+			getElementByXpath("btnDeleteUserGroup").click();
+		}
+		runJobSupervisor();
+	}
+
+	@Test(priority = 8)
+	public void deleteAllUserGroup() throws Exception {
+		getElementByXpath("btnUserGroup").click();
+		Utils.threadSleep(5000);
+		getElementByXpath("checkBoxAllUserGroup").click();
+		if (getElementByXpath("btnDeleteUserGroup").isEnabled()) {
+			getElementByXpath("btnDeleteUserGroup").click();
+		}
+		runJobSupervisor();
+	}
+
 }
--- src/com/suite/regression/Users.java
+++ src/com/suite/regression/Users.java
@@ -1,6 +1,9 @@
 package com.suite.regression;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
@@ -10,81 +13,96 @@ import org.testng.annotations.Test;
 import com.utils.Constants;
 import com.utils.Utils;
 
-public class Users extends RegressionSuiteBase{
-	
-  @BeforeTest
-  public void login(){
-	  getElementByXpath("textUserName").sendKeys(Constants.USERNAME);
-	  getElementByXpath("textPassWord").sendKeys(Constants.PASSWORD, Keys.ENTER);
-  }
-  
-  @AfterTest
-  public void logout(){
-	  getElementByXpath("btnAdministrator").click();
-	  getElementByXpath("btnLogout").click();
-  }
-  
-  public void userFormPage(){
-	  getElementByXpath("btnUser").click();
-	  getElementByXpath("btnAddDropDown").click();
-	  getElementByXpath("btnAddUser").click();
-  }
-	
-  @Test(priority=1)
-  public void createUser(){
-	  userFormPage();
-	  getElementByXpath("textUsersName").sendKeys("testuser" + Utils.randGen());
-	  getElementByXpath("textFirstName").sendKeys("test" + Utils.randGen());
-	  getElementByXpath("textLastName").sendKeys("user" + Utils.randGen());
-	  getElementByXpath("textEmail").sendKeys("testemail" + Utils.randGen() + "@openpeak.com");
-	  Select dropDown = new Select(getElementById("dropDownSelect"));
-	  dropDown.selectByVisibleText("India");
-	  getElementByXpath("btnScheduleNow").click();
-  }
-  
-  @Test(priority=2)
-  public void cancelCreatingUser(){
-	  userFormPage();
-	  getElementByXpath("textUsersName").sendKeys("testuser" + Utils.randGen());
-	  getElementByXpath("textFirstName").sendKeys("test" + Utils.randGen());
-	  getElementByXpath("textLastName").sendKeys("user" + Utils.randGen());
-	  getElementByXpath("textEmail").sendKeys("testemail" + Utils.randGen() + "@openpeak.com");
-	  Select dropDown = new Select(getElementById("dropDownSelect"));
-	  dropDown.selectByVisibleText("India");
-	  getElementByXpath("btnCancel").click();
-	  
-  }
-  
-  @Test(priority=3)
-  public void validateFormFields(){
-	  userFormPage();
-	  getElementByXpath("btnScheduleNow").click();
-	  Assert.assertEquals(getElementByXpath("emptyUserName").getText(), "This field is required.");
-	  Assert.assertEquals(getElementByXpath("emptyFirstName").getText(), "This field is required.");
-	  Assert.assertEquals(getElementByXpath("emptyLastName").getText(), "This field is required.");
-	  Assert.assertEquals(getElementByXpath("emptyEmail").getText(), "This field is required.");
-	  getElementByXpath("btnCancel").click();
-  }
-  
-  @Test(priority=4)
-  public void deleteUser(){
-	  createUser();
-	  getElementByXpath("checkBoxUser").click();
-	  if(getElementByXpath("btnDeleteUser").isEnabled()){
-		  getElementByXpath("btnDeleteUser").click();
-	  }
-	  runJobSupervisor();
-  }
-  
-  @Test(priority=5)
-  public void deleteAllUsers() throws Exception{
-	  getElementByXpath("btnUser").click();
-	  Utils.threadSleep(10000);
-	  getElementByXpath("checkBoxAllUsers").click();
-	  if(getElementByXpath("btnDeleteUser").isEnabled()){
-		  getElementByXpath("btnDeleteUser").click();
-	  }
-	  runJobSupervisor();
-  }
-  
+public class Users extends RegressionSuiteBase {
+
+	@BeforeTest
+	public void login() throws Exception {
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get(Constants.URL);
+		readOR(Constants.PATHOBJECTREPO);
+		getElementByXpath("textUserName").sendKeys(Constants.USERNAME);
+		getElementByXpath("textPassWord").sendKeys(Constants.PASSWORD,
+				Keys.ENTER);
+	}
+
+	@AfterTest
+	public void logout() {
+		getElementByXpath("btnAdministrator").click();
+		getElementByXpath("btnLogout").click();
+		driver.quit();
+	}
+
+	public void userFormPage() {
+		getElementByXpath("btnUser").click();
+		getElementByXpath("btnAddDropDown").click();
+		getElementByXpath("btnAddUser").click();
+	}
+
+	@Test(priority = 1)
+	public void createUser() {
+		userFormPage();
+		getElementByXpath("textUsersName").sendKeys(
+				"testuser" + Utils.randGen());
+		getElementByXpath("textFirstName").sendKeys("test" + Utils.randGen());
+		getElementByXpath("textLastName").sendKeys("user" + Utils.randGen());
+		getElementByXpath("textEmail").sendKeys(
+				"testemail" + Utils.randGen() + "@openpeak.com");
+		Select dropDown = new Select(getElementById("dropDownSelect"));
+		dropDown.selectByVisibleText("India");
+		getElementByXpath("btnScheduleNow").click();
+	}
+
+	@Test(priority = 2)
+	public void cancelCreatingUser() {
+		userFormPage();
+		getElementByXpath("textUsersName").sendKeys(
+				"testuser" + Utils.randGen());
+		getElementByXpath("textFirstName").sendKeys("test" + Utils.randGen());
+		getElementByXpath("textLastName").sendKeys("user" + Utils.randGen());
+		getElementByXpath("textEmail").sendKeys(
+				"testemail" + Utils.randGen() + "@openpeak.com");
+		Select dropDown = new Select(getElementById("dropDownSelect"));
+		dropDown.selectByVisibleText("India");
+		getElementByXpath("btnCancel").click();
+
+	}
+
+	@Test(priority = 3)
+	public void validateFormFields() {
+		userFormPage();
+		getElementByXpath("btnScheduleNow").click();
+		Assert.assertEquals(getElementByXpath("emptyUserName").getText(),
+				"This field is required.");
+		Assert.assertEquals(getElementByXpath("emptyFirstName").getText(),
+				"This field is required.");
+		Assert.assertEquals(getElementByXpath("emptyLastName").getText(),
+				"This field is required.");
+		Assert.assertEquals(getElementByXpath("emptyEmail").getText(),
+				"This field is required.");
+		getElementByXpath("btnCancel").click();
+	}
+
+	@Test(priority = 4)
+	public void deleteUser() {
+		createUser();
+		getElementByXpath("checkBoxUser").click();
+		if (getElementByXpath("btnDeleteUser").isEnabled()) {
+			getElementByXpath("btnDeleteUser").click();
+		}
+		runJobSupervisor();
+	}
+
+	@Test(priority = 5)
+	public void deleteAllUsers() throws Exception {
+		getElementByXpath("btnUser").click();
+		Utils.threadSleep(10000);
+		getElementByXpath("checkBoxAllUsers").click();
+		if (getElementByXpath("btnDeleteUser").isEnabled()) {
+			getElementByXpath("btnDeleteUser").click();
+		}
+		runJobSupervisor();
+	}
+
 }
